3796ec50fa8a579261f4f7082ba810ca74f594a8
joeiyeo <Joeyyeo221@gmail.com>
2025-03-26 11:49:43 +0800

    Add files via upload

---
+++ OKUCare/game.js
@@ -0,0 +1,27 @@
+﻿var config = {
+    type: Phaser.AUTO,
+    // pixel size * tile map size * zoom 
+    width: 960,
+    height: 800,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    backgroundColor: '#1128a9',
+    pixelArt: true,
+    scene: [preload, main , lvl1Scr, lvl2Scr, lvl3Scr, level1, level2, level3, winningScr, gameover, showInventory]
+ 
+};
+
+var game = new Phaser.Game(config);
+window.item = 0
+window.heart = 3
+window.key = 0
+
+
---
+++ OKUCare/globalFunctions.js
@@ -0,0 +1,109 @@
+﻿////////////////////////////////////////////////////////
+//
+// access this function using updateInventory.call(this)
+// Uses a JS function to prevent repeated codes
+// 
+///////////////////////////////////////////////////////
+function updateInventory() {
+    console.log("*** updateInventory()")
+    // Emit events showInventory
+    this.inventory = {}
+    this.inventory.key = window.key
+    this.inventory.heart = window.heart
+     
+    console.log('*** updateInventory() Emit event', this.inventory)
+    this.invEvent = (event, data) =>  { 
+			    this.scene.get('showInventory').events.emit(event, data); 
+		    }
+    this.invEvent("inventory", this.inventory);
+  }
+  
+  ////////////////////////////////////////////////////////
+  //
+  // access this function with globalHitFire
+  // Uses a JS function to prevent repeated codes
+  // 
+  ///////////////////////////////////////////////////////
+  function globalHitFire1(player,item) {
+      console.log("*** player overlap fire");
+     
+      // Shake screen
+     this.cameras.main.shake(100);
+     //this.hitenemySnd.play();
+  
+		  // deduct heart
+      window.heart--;
+      item.disableBody(true, true);
+      
+      // Call globalFunctions.js updateInventory
+      updateInventory.call(this)
+ 
+    if (window.heart == 0){
+	    console.log("*** player gameosver");
+      this.scene.start("gameover");
+      //this.loselifeSnd.play();
+    }
+  }
+
+
+  function globalHitFire2(player,item) {
+    console.log("*** player overlap fire");
+   
+    // Shake screen
+   this.cameras.main.shake(100);
+   //this.hitenemySnd.play();
+
+    // deduct heart
+    window.heart--;
+    item.disableBody(true, true);
+    
+    // Call globalFunctions.js updateInventory
+    updateInventory.call(this)
+
+  if (window.heart == 0){
+    console.log("*** player gameosver");
+    this.scene.start("gameover");
+    //this.loselifeSnd.play();
+  }
+}
+
+function globalHitFire3(player,item) {
+  console.log("*** player overlap fire");
+ 
+  // Shake screen
+ this.cameras.main.shake(100);
+ //this.hitenemySnd.play();
+
+  // deduct heart
+  window.heart--;
+  item.disableBody(true, true);
+  
+  // Call globalFunctions.js updateInventory
+  updateInventory.call(this)
+
+if (window.heart == 0){
+  console.log("*** player gameosver");
+  this.scene.start("gameover");
+  //this.loselifeSnd.play();
+}
+}
+  
+  ////////////////////////////////////////////////////////
+  //
+  // access this function with globalCollectKey
+  // Uses a JS function to prevent repeated codes
+  // 
+  /////////////////////////////////////////////////////// 
+ function globalCollectKey(player,item) {
+    console.log("*** player overlap key");
+   
+    // Shake screen
+   this.cameras.main.shake(100);
+
+   //this.hitenemySnd.play();
+
+	// increase key count
+    window.key++;
+    item.disableBody(true, true);
+    updateInventory.call(this)
+}
